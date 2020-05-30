17f56e32a057839733ef66395456f6d0c918afc9
joaquin lema <joaco.lema@hotmail.com>
2020-05-30 17:38:17 -0300

    agregamos preloader a log

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import LogItem from './LogItem';
+import Preloader from '../layout/Preloader';
 
 const Logs = () => {
 
@@ -20,7 +21,7 @@ const Logs = () => {
     }
 
     if(loading){
-        return <h4>loading</h4>
+        return <Preloader />
     }
 
     return (
