f0ccbfa5ed7e4bef461cd1350810de203cb3680c
joaquin lema <joaco.lema@hotmail.com>
2020-05-30 18:27:18 -0300

    agregamos un modal para agregar un item por consola

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import M from 'materialize-css/dist/js/materialize.min.js';
 import SearchBar from  './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
 import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
 import './App.css';
 
 function App() {
@@ -13,10 +14,15 @@ function App() {
     M.AutoInit();
   });
 
+  const addLog = (item) => {
+    console.log(item);
+  }
+
   return (
     <Fragment>
       <SearchBar />
       <div className='container'> 
+        <AddLogModal addLog={addLog}/>
         <AddBtn />
         <Logs />
       </div>
