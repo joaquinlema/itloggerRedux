eb10a4a607a1db8a58758a1884db5d2014d964c3
joaquin lema <joaco.lema@hotmail.com>
2020-05-30 17:29:53 -0300

    agregamos datos extra para mostrar con moment

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Moment from 'react-moment';
 
 
 const LogItem = ({log}) => {
@@ -9,6 +10,17 @@ const LogItem = ({log}) => {
                 <a href="#edit-log-modal" className={`modal-trigger ${log.completed ? 'red-text' : 'blue-text'}`}
                 >
                     {log.title}</a>
+                <span className='grey-text'>
+                    <span className='black-text'>
+                        Id: #{log.id}
+                    </span>
+                    <span className='black-text'>
+                        User id: #{log.userId}
+                    </span>
+                    <span className='black-text'>
+                        Last Comments on <Moment format='MMMM Do YYYY, h:mm:ss a'>{new Date()}</Moment>
+                    </span>
+                </span>
             </div>
         </li>
     );
