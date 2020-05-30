faa2dff2d3e9121e79c3b942e6dc83bf942ec694
joaquin lema <joaco.lema@hotmail.com>
2020-05-30 15:16:41 -0300

    agregamos material icons via link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    //initialize material js para usar todo de materialize
+    M.AutoInit();
+  },[])
+  
   return (
     <div className="App">
      
