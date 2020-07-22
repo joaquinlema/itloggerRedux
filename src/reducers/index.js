a19e6faadbd16cd1c43e62c5160dbff32834b5a4
Joaquin Lema <joaquin.lema@softtek.com>
2020-07-22 15:35:31 -0300

    importamos y agregamos el logReducer a nuestro reducer general

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,7 @@
 import { combineReducers} from 'redux';
+import logReducer from './logReducer';
 
 //exporto un conjunto con todos los reducers de mi app
-export default  combineReducers({});
+export default  combineReducers({
+    log: logReducer
+});
